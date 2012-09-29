e83cf48604f9a5587a37ef093cbf39a7f8c42e5a
Vig Sachidananda <v.sachidananda@gmail.com>
2012-09-29 18:31:32 -0400

    Initial Commit

---
+++ SocialPollster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SocialPollster
+//
+//  Created by Vig Sachidananda on 9/29/12.
+//  Copyright (c) 2012 Vig Sachidananda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
